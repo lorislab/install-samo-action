e8a5b1af0f8fce87122dfe98015bf82bac352696
Andrej Petras <andrej@lorislab.org>
2023-01-18 19:17:49 +0100

    feat: create install samo action

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# install-samo-action
-Install samo tool for github action
+## Example usage
+
+```
+- name: Install samo
+  uses: lorislab/install-samo-action@v1
+  with:
+    version: "3.3.0"
+```
+
+## Development
+
+#### Local development
+
+Install the dependencies
+
+```bash
+npm install
+```
+
+#### Package for distribution
+
+
+Run prepare
+
+```bash
+npm run prepare
+```
+
+Since the packaged index.js is run from the dist folder.
+
+```bash
+git add dist
+```
+
+#### Create a release branch
+
+Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
+
+Checkin to the v1 release branch
+
+```bash
+git checkout -b v1
+git commit -a -m "v1 release"
+```
+
+```bash
+git push origin v1
+```
+
+Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
+
+Your action is now published! :rocket:
+
+See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
+
+
+async function run() {
+
+  try {
+    
+    let version = core.getInput('version');
+    let downloadURL = core.getInput('download_url');
+
+    const fillTemplate = function(s) {
+      s = s.replace(/\$\{version\}/g, version)
+      return s
+    }
+    downloadURL = fillTemplate(downloadURL);
+    core.info(`samo donwload URL: ${downloadURL}`)
+    core.info(`smoke test:        ${test}`)
+    
+    await installTool(version, downloadURL);    
+    await exec.exec('samo version');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function installTool(version, url) {
+  let cachedPath = tc.find('samo', version)
+  if (cachedPath) {
+      core.addPath(cachedPath)
+      return
+  }
+
+  const file = await tc.downloadTool(url);
+
+  await exec.exec(`mkdir samo`)
+  await exec.exec(`tar -C samo -xzvf ${file} --strip-components 0 --wildcards samo`)
+
+  cachedPath = await tc.cacheDir('samo', 'samo', version);
+  core.addPath(cachedPath)
+}
+
+run();
